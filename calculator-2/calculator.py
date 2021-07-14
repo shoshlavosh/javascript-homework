817f5c4b37dcf16f2f257283bb0a243888cb20dd
shoshlavosh <shoshlavosh@gmail.com>
2021-07-14 23:13:18 +0000

    functions up to square done

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -11,24 +11,43 @@ while True:
     # input_string = int(input_string)
 #     tokenize input
     tokens = input_string.split(" ")
+    # num1=int(tokens[1])
+    # num2=0
+    # if len(tokens) == 3:
+    #     num2 = int(tokens[2]) 
+
     if tokens[0] == "q":
         break
     
     elif tokens[0] == "+":
         #call add function
-        print(str(add(int(tokens[1]),int(tokens[2]))))
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
+        print(str(add(num1, num2)))
         
     elif tokens[0] == "-":
         #call subtraction function
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
+        print(str(subtract(num1, num2)))
+
 
     elif tokens[0] == "*":
         #call multiplication function
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
+        print(str(multiply(num1, num2)))
 
     elif tokens[0] == "/":
         #call division function
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
+        print(str(divide(num1, num2)))
 
     elif tokens[0] == "square":
         #call square function
+        num1 = int(tokens[1])
+        print(str(square(num1)))
 
     elif tokens[0] == "cube":
         #call cube function
