5100e5ce831451d624bbaf63b91cad92d3220f42
shoshlavosh <shoshlavosh@gmail.com>
2021-07-14 23:06:00 +0000

    first draft of calculator 2

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -11,34 +11,34 @@ while True:
     # input_string = int(input_string)
 #     tokenize input
     tokens = input_string.split(" ")
-        if tokens[0] == "q":
-            break
+    if tokens[0] == "q":
+        break
+    
+    elif tokens[0] == "+":
+        #call add function
+        print(str(add(int(tokens[1]),int(tokens[2]))))
         
-        elif token[0] == "+":
-            #call add function
-            add(int(token[1]),int(token[2]))
+    elif tokens[0] == "-":
+        #call subtraction function
 
-        elif token[0] == "-":
-            #call subtraction function
+    elif tokens[0] == "*":
+        #call multiplication function
 
-        elif token[0] == "*":
-            #call multiplication function
+    elif tokens[0] == "/":
+        #call division function
 
-        elif token[0] == "/":
-            #call division function
+    elif tokens[0] == "square":
+        #call square function
 
-        elif token[0] == "square":
-            #call square function
+    elif tokens[0] == "cube":
+        #call cube function
 
-        elif token[0] == "cube":
-            #call cube function
+    elif tokens[0] == "pow":
+        #call power function
 
-        elif token[0] == "pow":
-            #call power function
+    elif token[0] == "mod":
+        #call modulo function
 
-        elif token[0] == "mod":
-            #call modulo function
-
-        else:
-            print("That entry is not valid.\n")
+    else:
+        print("That entry is not valid.\n")
 
