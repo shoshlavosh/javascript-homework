3d2abaaf6f2ea852f062885bf90fe5e7271e6e64
shoshlavosh <shoshlavosh@gmail.com>
2021-07-15 00:24:36 +0000

    Updates made by Shannon to streamline the code

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -10,57 +10,66 @@ while True:
     input_string = input("Please enter an operation > ")
     # input_string = int(input_string)
     # tokenize input
-    tokens = input_string.split(" ")
-    # num1=int(tokens[1])
-    # num2=0
-    # if len(tokens) == 3:
-    #     num2 = int(tokens[2]) 
+    tokens = input_string.split(" ") # tokens is a list ['first input', 'second input', 'third input', etc...]
+    first_input = tokens.pop[0]
+    """ list.pop([i])
+    Remove the item at the given position in the list, and return it
+    """
 
-    if tokens[0] == "q":
+    for i in tokens:
+        tokens[i] = int(tokens[i])
+
+    # check that the human entered at most 10 entries
+    # if len(tokens) > 10:
+    #     print("Enter fewer integers, please.")
+    #     continue
+
+    if first_input == "q":
         break
     
-    elif tokens[0] == "+":
+    elif first_input == "+":
         #call add function
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
-        print(str(add(num1, num2)))
+        sum_num = tokens.pop[0]
+        for i in tokens:
+            sum_num = add(sum_num, i)
+        print(str(sum_num))
         
-    elif tokens[0] == "-":
+    elif first_input == "-":
         #call subtraction function
         num1 = int(tokens[1])
         num2 = int(tokens[2])
         print(str(subtract(num1, num2)))
 
 
-    elif tokens[0] == "*":
+    elif first_input == "*":
         #call multiplication function
         num1 = int(tokens[1])
         num2 = int(tokens[2])
         print(str(multiply(num1, num2)))
 
-    elif tokens[0] == "/":
+    elif first_input == "/":
         #call division function
         num1 = int(tokens[1])
         num2 = int(tokens[2])
         print(str(divide(num1, num2)))
 
-    elif tokens[0] == "square":
+    elif first_input == "square":
         #call square function
         num1 = int(tokens[1])
         print(str(square(num1)))
 
-    elif tokens[0] == "cube":
+    elif first_input == "cube":
         #call cube function
         num1 = int(tokens[1])
         print(str(cube(num1)))
 
-    elif tokens[0] == "pow":
+    elif first_input == "pow":
         #call power function
         num1 = int(tokens[1])
         num2 = int(tokens[2])
         print(str(power(num1, num2)))
 
-    elif tokens[0] == "mod":
+    elif first_input == "mod":
         #call modulo function
         num1 = int(tokens[1])
         num2 = int(tokens[2])
